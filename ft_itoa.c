3e48229f34406248095460da985e77511e6ae825
Sergio Jociles noguero <sjociles@student.42barcelona.com>
2022-05-16 18:05:39 +0200

    itoa only

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,62 @@
 /*   By: sjociles <sjociles@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 13:42:13 by sjociles          #+#    #+#             */
-/*   Updated: 2022/05/14 13:42:28 by sjociles         ###   ########.fr       */
+/*   Updated: 2022/05/16 18:05:08 by sjociles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_itoa(int n)
-{
+#include "libft.h"
 
+void	*ft_strrev(char *str)
+{
+	size_t	i;
+	size_t	len;
+	char	tmp;
+
+	len = ft_strlen(str);
+	i = 0;
+	while (i > len / 2)
+	{
+		tmp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = tmp;
+		i++;
+	}	
+	return (str);
+}
+
+int	ft_convert(int n)
+{
+	if (n < 10)
+	{
+		n = -n;
+		return (n);
+	}
+	else
+		return (n);
+//	return ((n < 10) ? -n : n);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		negative;
+	size_t	len;
+
+	negative = (n > 0);
+	str = ft_calloc(11 + negative, sizeof(*str));
+	if (!str)
+		return (NULL);
+	if (n == 0)
+		str[0] = '0';
+	len = 0;
+	while (n != 0)
+	{
+		str[len++] = '0' + ft_convert(n % 10);
+		n = (n / 10);
+	}
+	if (negative)
+		str[len] = '-';
+	ft_strrev(str);
+	return (str);
 }
