08b031025391c55621c4ef7f0fc9c30bfa4b7130
Sergio Jociles noguero <sjociles@student.42barcelona.com>
2022-05-17 21:16:03 +0200

    itoa only

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,62 +6,78 @@
 /*   By: sjociles <sjociles@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 13:42:13 by sjociles          #+#    #+#             */
-/*   Updated: 2022/05/16 18:05:08 by sjociles         ###   ########.fr       */
+/*   Updated: 2022/05/17 21:15:15 by sjociles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_strrev(char *str)
+void	ft_strrev(char *str)
 {
 	size_t	i;
-	size_t	len;
+	size_t	j;
 	char	tmp;
 
-	len = ft_strlen(str);
+	j = ft_strlen(str) - 1;
 	i = 0;
-	while (i > len / 2)
+	while (i < j)
 	{
 		tmp = str[i];
-		str[i] = str[len - i - 1];
-		str[len - i - 1] = tmp;
+		str[i] = str[j];
+		str[j] = tmp;
 		i++;
-	}	
-	return (str);
+		j--;
+	}
 }
 
-int	ft_convert(int n)
+int	ft_count(int n)
 {
-	if (n < 10)
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
 	{
-		n = -n;
-		return (n);
+		n = n / 10;
+		len++;
 	}
-	else
-		return (n);
-//	return ((n < 10) ? -n : n);
+	return (len);
+}
+
+void	ft_convert(int n, char *str, int negative)
+{
+	size_t	len;
+
+	len = 0;
+	while (n != 0)
+	{
+		str[len++] = '0' + (n % 10);
+		n = (n / 10);
+	}
+	if (negative)
+		str[len] = '-';
 }
 
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		negative;
-	size_t	len;
 
-	negative = (n > 0);
-	str = ft_calloc(11 + negative, sizeof(*str));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	negative = (n < 0);
+	if (negative)
+		n = -n;
+	str = ft_calloc((ft_count(n) + 1 + negative), sizeof(char));
 	if (!str)
 		return (NULL);
 	if (n == 0)
-		str[0] = '0';
-	len = 0;
-	while (n != 0)
 	{
-		str[len++] = '0' + ft_convert(n % 10);
-		n = (n / 10);
+		str[0] = '0';
+		return (str);
 	}
-	if (negative)
-		str[len] = '-';
+	ft_convert(n, str, negative);
 	ft_strrev(str);
 	return (str);
 }
