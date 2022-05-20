eaaceb842ac57025496caf7ebac9f3511ddf1baf
Sergio Jociles noguero <sjociles@student.42barcelona.com>
2022-05-20 18:06:03 +0200

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: sjociles <sjociles@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:18:53 by sjociles          #+#    #+#             */
-/*   Updated: 2022/05/18 16:31:26 by sjociles         ###   ########.fr       */
+/*   Updated: 2022/05/20 18:05:09 by sjociles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*elem;
+	void	*content;
 
 	if (!lst)
 		return (0);
 	new_lst = 0;
 	while (lst)
 	{
-		elem = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		elem = ft_lstnew(content);
 		if (!elem)
 		{
+			if (content)
+				free(content);
 			ft_lstclear(&new_lst, del);
 			return (0);
 		}
